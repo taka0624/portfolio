ef401110581fa35ef83c3ccb85647cad3869bb5b
taka0624 <knpqx398@gmail.com>
2021-10-03 15:38:05 +0900

    add article.html実装 fix headerのcolor設定修正

--- base/assets/js/index.js
+++ base/assets/js/index.js
@@ -2,9 +2,9 @@ $(function () {
   $(window).on("scroll", function () {
     const sliderHeight = $(".firstView").height();
     if (sliderHeight - 30 < $(this).scrollTop()) {
-      $(".js-header").addClass("headerColorScroll");
-    } else {
       $(".js-header").removeClass("headerColorScroll");
+    } else {
+      $(".js-header").addClass("headerColorScroll");
     }
   });
 });
