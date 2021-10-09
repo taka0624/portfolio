17a763134d0cfc399285985c8de76614171b71d6
taka0624 <knpqx398@gmail.com>
2021-10-09 16:16:59 +0900

    add 各ページphpファイル追加

---
+++ wp-content/themes/portfolio/single.php
@@ -0,0 +1,38 @@
+<?php get_header(); ?>
+	<main class="singleWrapper">
+    <div class="container">
+      <h2 class="title">
+        制作名称
+      </h2>
+      <div class="deliverableWrap">
+        <div class="imgWrap">
+          <a href="">
+            <div class="bgImg thumbnail"></div>
+          </a>
+        </div>
+        <dl class="content">
+          <dt class="heading">
+            制作期間：
+          </dt>
+          <dd>
+            あああああああああ
+          </dd>
+          <dt class="heading">
+            制作内容：
+          </dt>
+          <dd>
+            いいいいいいいいいい
+          </dd>
+          <dt class="heading">
+            制作物URL：
+          </dt>
+          <dd>
+            <a href="">
+              https
+            </a>
+          </dd>
+        </dl>
+      </div>
+    </div>
+  </main>
+<?php get_footer(); ?>
