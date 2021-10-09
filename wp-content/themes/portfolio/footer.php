4aee9d2fdf64831855680aca2dd90f4814e56861
taka0624 <knpqx398@gmail.com>
2021-10-09 14:35:32 +0900

    add wp準備

---
+++ wp-content/themes/portfolio/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+    <div class="inner">
+      <p class="copyright">
+        <small>
+          &copy; 2021 KAWAKAMI TAKAHIRO
+        </small>
+      </p>
+    </div>
+  </footer>
+<?php wp_footer(); ?>
+  <script src="assets/js/jquery-3.5.1.min.js"></script>
+  <script src="assets/js/index.js"></script>
+</body>
+</html>
