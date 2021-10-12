2c66a6f769746ee1d4034b907c68888d4a988720
taka0624 <knpqx398@gmail.com>
2021-10-12 11:44:21 +0900

    [add] ciontact.page実装

--- wp-content/themes/portfolio/page-contact_complete.php
+++ wp-content/themes/portfolio/page-contact_complete.php
@@ -4,10 +4,9 @@
       <h2 class="title">
         Contact
       </h2>
-      <p class="text">
-        お問い合わせの送信が完了しました。<br>
-        後日担当者よりご返信いたしますので、しばらくお待ちください。
-      </p>
+<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+<?php echo the_content(); ?>
+<?php endwhile; endif; ?>
       <p class="btnWrap">
         <a class="btn primaryBtn" href="#">
           Top Page
